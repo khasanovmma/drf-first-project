a08cb0c7bd58ba3045c90ada6d97ac1a601c47fa
khasanovmma <khasanovmma010@gmail.com>
2022-06-16 15:14:17 +0500

    use routers Simple and Default, add rout category

--- man/urls.py
+++ man/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from rest_framework import routers
 from man.views import ManViewSet
 
-router = routers.SimpleRouter()
-router.register('man', ManViewSet)
+router = routers.DefaultRouter()
+router.register('man', ManViewSet)  # param: basename -> name url
 
 urlpatterns = [
-    path('', include(router.urls)), # http://127.0.0.1:8000/api/v1/man/
+    path('', include(router.urls)),  # http://127.0.0.1:8000/api/v1/man/
 ]
--- man/views.py
+++ man/views.py
@@ -1,8 +1,16 @@
 from rest_framework import viewsets
-from man.models import Man
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from man.models import Man, Category
 from man.serializer import ManSerializer
 
 
 class ManViewSet(viewsets.ModelViewSet):
     queryset = Man.objects.all()
     serializer_class = ManSerializer
+
+    @action(methods=['get'], detail=False)
+    def category(self, request):
+        categories = Category.objects.all()
+        return Response({'categorY_list': [category.title for category in categories ]})
